7ac6141e1fb8f4f1cba32a6528c0231d5faf5699
jamesbowman <jamesbowman@7e7b20ed-14cf-457c-ab63-26a741536580>
2010-11-21 16:37:52 +0000

    Cookbook example
    git-svn-id: https://svn.code.sf.net/p/openexrpython/code@30 7e7b20ed-14cf-457c-ab63-26a741536580

---
+++ cook1.py
@@ -0,0 +1,19 @@
+import OpenEXR
+import Imath
+import Image
+
+file = OpenEXR.InputFile("Chunks.exr")
+pt = Imath.PixelType(Imath.PixelType.FLOAT)
+dw = file.header()['dataWindow']
+size = (dw.max.x - dw.min.x + 1, dw.max.y - dw.min.y + 1)
+
+rgbf = [Image.fromstring("F", size, file.channel(c, pt)) for c in "RGB"]
+
+extrema = [im.getextrema() for im in rgbf]
+darkest = min([lo for (lo,hi) in extrema])
+lighest = max([hi for (lo,hi) in extrema])
+scale = 255 / (lighest - darkest)
+def normalize_0_255(v):
+    return (v * scale) + darkest
+rgb8 = [im.point(normalize_0_255).convert("L") for im in rgbf]
+Image.merge("RGB", rgb8).save("foo.jpg")
